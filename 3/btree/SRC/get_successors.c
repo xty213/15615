088c54ec8ecea4b89945a1dd4901b154bb48efa7
Tianyu Xu <xty213@gmail.com>
2015-02-15 16:27:39 -0500

    finish get_succesors()

--- 3/btree/SRC/get_successors.c
+++ 3/btree/SRC/get_successors.c
@@ -1,12 +1,54 @@
 #include "def.h"
 
+// external functions
+extern int check_word(char *word);
+extern int strtolow(char *s);
+extern int FindInsertionPosition(struct KeyRecord * KeyListTraverser, char *Key, int *Found, NUMKEYS NumKeys, int Count);
+extern struct PageHdr *FetchPage(PAGENO Page);
+extern PAGENO treesearch_page(PAGENO PageNo, char *key);
 
 int get_successors(char *key, int k, char *result[]) {
-    int cnt = 0;
+    if (check_word(key) == FALSE) {
+        return 0;
+    }
+    strtolow(key);
 
-    // try to find successors
-    for (; cnt < k; cnt++) {
-        result[cnt] = "alexander";
+    int cnt = 0; // found successors number
+    int Count = 0, Found, i; // helper variables for FindInsertionPosition
+
+    // find the first successor
+    PAGENO page = treesearch_page(ROOT, key);
+    struct PageHdr *PagePtr = FetchPage(page);
+    struct KeyRecord *KeyListTraverser = PagePtr->KeyListPtr;
+    int InsertionPosition = FindInsertionPosition(KeyListTraverser, key, &Found, PagePtr->NumKeys, Count);
+    for (i = 0; i < InsertionPosition; i++)
+            KeyListTraverser = KeyListTraverser->Next;
+    if (KeyListTraverser == NULL) {
+        page = PagePtr->PgNumOfNxtLfPg;
+        if (page != NULLPAGENO) {
+            PagePtr = FetchPage(page);
+            KeyListTraverser = PagePtr->KeyListPtr;
+        }
+    }
+
+    // keep finding successors
+    while (cnt < k && KeyListTraverser != NULL) {
+        // save the current successor
+        result[cnt] = KeyListTraverser->StoredKey;
+        // if the current page is over, read next logical page
+        if (KeyListTraverser->Next == NULL) {
+            page = PagePtr->PgNumOfNxtLfPg;
+            // next logical page does not exist
+            if (page == NULLPAGENO) {
+                break;
+            } else {
+                PagePtr = FetchPage(page);
+                KeyListTraverser = PagePtr->KeyListPtr;
+            }
+        } else {
+            KeyListTraverser = KeyListTraverser->Next;
+        }
+        cnt++;
     }
 
     printf("found %d successors:\n", cnt);
