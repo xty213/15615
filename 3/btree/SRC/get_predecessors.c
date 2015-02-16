cc121759e3bc1d04b4af9128672f1ef9c92f2538
Tianyu Xu <xty213@gmail.com>
2015-02-15 22:13:20 -0500

    temp commit - use backtrack

--- 3/btree/SRC/get_predecessors.c
+++ 3/btree/SRC/get_predecessors.c
@@ -2,7 +2,16 @@
 #include <string.h>
 #include "def.h"
 
+// external functions
+extern int check_word(char *word);
+extern int strtolow(char *s);
+extern int FindInsertionPosition(struct KeyRecord * KeyListTraverser, char *Key, int *Found, NUMKEYS NumKeys, int Count);
+extern struct PageHdr *FetchPage(PAGENO Page);
+PAGENO FindPageNumOfChild(struct PageHdr *PagePtr, struct KeyRecord *KeyListTraverser, char *Key, NUMKEYS NumKeys)
+
 int get_predecessors(char *key, int k, char *result[]) {
+    void find_predecessors(PAGENO PageNo, char *key, int k, char *result[], int *cnt);
+    
     if (check_word(key) == FALSE) {
         return 0;
     }
@@ -21,7 +30,7 @@ void find_predecessors(PAGENO PageNo, char *key, int k, char *result[], int *cnt
     // if is leaf, try to add records in this leaf node
     if (IsLeaf(PagePtr)) {
         pos = FindInsertionPosition(PagePtr->KeyListPtr, key, &Found, PagePtr->NumKeys, 0);
-        for (i = pos; i >= 0 && k > cnt; i--) {
+        for (i = pos; i >= 0 && k > *cnt; i--) {
             struct KeyRecord *keyRecord = PagePtr->KeyListPtr;
             for (j = 0; j < i; j++) {
                 keyRecord = keyRecord->Next;
