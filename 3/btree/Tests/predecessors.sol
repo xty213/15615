4700f41d39fa369b7e8dc77536bf06a82b9794c5
Tianyu Xu <xty213@gmail.com>
2015-02-16 14:06:53 -0500

    add predecessors test

--- 3/btree/Tests/predecessors.sol
+++ 3/btree/Tests/predecessors.sol
@@ -1,4 +1,91 @@
-you shall not pass,
-implement the get k predecessors correctly first,
-and then update this solution file.
-"test early, test often, test automatically".
+
+    *** These are your commands .........
+    "C" to scan the tree
+    "i" to insert
+    "p" to print a btree page
+    "s" to search, and print the key
+    "S" to search, and print the key, posting list pairs
+    ">" to print k successors
+    "<" to print k predecessors
+    "T" to print the btree in inorder format
+    "#" to reset and print stats
+    "x" to exit
+word=?
+k=?
+found 5 predecessors:
+hercogamy
+herculanean
+herculanensian
+herculanian
+herculean
+
+    *** These are your commands .........
+    "C" to scan the tree
+    "i" to insert
+    "p" to print a btree page
+    "s" to search, and print the key
+    "S" to search, and print the key, posting list pairs
+    ">" to print k successors
+    "<" to print k predecessors
+    "T" to print the btree in inorder format
+    "#" to reset and print stats
+    "x" to exit
+word=?
+k=?
+found 5 predecessors:
+onomatologist
+onomatology
+onomatomania
+onomatope
+onomatoplasm
+
+    *** These are your commands .........
+    "C" to scan the tree
+    "i" to insert
+    "p" to print a btree page
+    "s" to search, and print the key
+    "S" to search, and print the key, posting list pairs
+    ">" to print k successors
+    "<" to print k predecessors
+    "T" to print the btree in inorder format
+    "#" to reset and print stats
+    "x" to exit
+    *** These are your commands .........
+    "C" to scan the tree
+    "i" to insert
+    "p" to print a btree page
+    "s" to search, and print the key
+    "S" to search, and print the key, posting list pairs
+    ">" to print k successors
+    "<" to print k predecessors
+    "T" to print the btree in inorder format
+    "#" to reset and print stats
+    "x" to exit
+word=?
+k=?
+found 5 predecessors:
+sesquinonal
+sesquioctava
+sesquioctaval
+sesquioxide
+sesquipedal
+
+    *** These are your commands .........
+    "C" to scan the tree
+    "i" to insert
+    "p" to print a btree page
+    "s" to search, and print the key
+    "S" to search, and print the key, posting list pairs
+    ">" to print k successors
+    "<" to print k predecessors
+    "T" to print the btree in inorder format
+    "#" to reset and print stats
+    "x" to exit
+word=?
+k=?
+found 5 predecessors:
+malleolar
+malleolus
+mallet
+malleus
+malling
